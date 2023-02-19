ab756817ec7367f59c4d4057218de040300a11cd
luongnv <vanluongnbbl@gmail.com>
2023-02-19 20:51:11 +0700

    learning file-system routing

---
+++ pages/about.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+
+export interface AboutPageProps {
+}
+
+export default function AboutPage (props: AboutPageProps) {
+  return (
+    <div>
+        <h1>About Page</h1>
+    </div>
+  );
+}
---
+++ pages/posts/create.tsx
@@ -0,0 +1,10 @@
+import * as React from 'react';
+
+export interface CreatePageProps {
+}
+
+export default function CreatePostPage (props: CreatePageProps) {
+  return (
+    <div>Create list page</div>
+  );
+}
